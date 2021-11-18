acfe2845e25337155cc715d1ff7788a4a77561d5
kipit <k_github@42l.fr>
2021-11-18 17:40:27 +0100

    change smartcontract for ERC1155 one

--- contracts/TheForge.sol
+++ contracts/TheForge.sol
@@ -1,93 +1,88 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
-import "./VerifySignature.sol";
+contract TheForge is ERC1155, AccessControl, Pausable, ERC1155Burnable {
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    
+    mapping(address => uint) private invitation;
 
-contract TheForge is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
-    
-    string _URI = "https://gateway.pinata.cloud/ipfs/QmWoaBpmunVsVMqVXBos62f2YTTTV4V9EE2cfvVaL3aNRR";
-    
-    //Import external contract to verify signature
-    address public AddressVS;
-    
-    function _setAddressVerifySignature(address _AddressVS) public {
-        AddressVS = _AddressVS;
+    constructor()
+        ERC1155("ipfs://QmWoaBpmunVsVMqVXBos62f2YTTTV4V9EE2cfvVaL3aNRR")
+    {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(URI_SETTER_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
     }
-    
-    function callVerify(address _signer, address _to, bytes memory signature) public view returns (bool){
-        require(AddressVS != address(0));
-        VerifySignature vs = VerifySignature(AddressVS);
-        return vs.verify(_signer, _to, signature);
+
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        _setURI(newuri);
     }
-    
-    // Mapping from address to Anvil ID
-    mapping(address => uint256) private _ownersAnvil;
-    
-    // Mapping from address to Hammer ID
-    mapping(address => uint256) private _ownersHammer;
-    
-    // Mapping from address to Invite Score, if 0 then the user cannot invite more people
-    mapping(address => uint256) private _inviteScore;
 
-    constructor() ERC721("TheForgeHammer", "TFH") {}
-
-    function pause() public onlyOwner {
+    function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
-
-    function safeMint(address to, address invintingAddress, bytes memory invitationSignature) public {
+    
+    function hasInvite(address account) public view returns (uint) {
+        return invitation[account];
+    }
+    
+    function addInvite(address[] memory accounts, uint256[] memory amounts)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        require(accounts.length == amounts.length, "Input error - accounts and amounts length mismatch");
         
-        //Check that the invitation was indeed created by the invintingAddress and that the address 'to' is the invitee by comparing these inputs to the signature
-        require(callVerify(invintingAddress, to, invitationSignature));
-
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, _URI);
+        for(uint256 i = 0; i < accounts.length; i++) {
+            invitation[accounts[i]] += amounts[i];
+        }
+    }
+    
+    function resetInvite(address[] memory accounts) public onlyRole(MINTER_ROLE) {
+        for(uint256 i = 0; i < accounts.length; i++) {
+            invitation[accounts[i]] = 0;
+        }
+    }
+    
+    function mint(address invitee) public {
+        require(invitation[msg.sender] > 0, "No available invitation");
+        
+        invitation[msg.sender]--;
+        _mint(invitee, 0, 1, "");
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    function forceMint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
         whenNotPaused
-        override(ERC721, ERC721Enumerable)
+        override
     {
-        super._beforeTokenTransfer(from, to, tokenId);
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC1155, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,9 +6,6 @@ const main = async () => {
     await contract.deployed();
     console.log("Contract is deployed on:", contract.address);
 
-    let setAddress = await contract._setAddressVerifySignature("0xb856f896c560eF1e1CdBad9F66BeB3c8B264Fd26");
-
-    await setAddress.wait();
 
     // let minting2 = await contract.makeAnEpicNFT();
 
