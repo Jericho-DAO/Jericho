159841bd86aa7358b74512d5ed40a466e6173f0f
kipit <k_github@42l.fr>
2021-11-18 20:01:25 +0100

    added anvil gate keeping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,10 @@ import TheRegister from './components/TheRegister.js';
 import { ConnectWallet } from './components/ConnectWallet';
 import { NetworkErrorMessage } from './components/NetworkErrorMessage'; 
 import TheForge from "./contracts/TheForge.json";
+import Anvil from "./contracts/AnvilTheForge.json";
 
 const CONTRACT_ADDRESS_THEFORGE = "0xE5868B98E594510788F469ec5ca0440FCAb05873";
+const CONTRACT_ADDRESS_ANVIL = "0x619f82bf98423cd8BD31B5Cd26BeCBA095EC2b43";
 
 function App() {
   
@@ -17,6 +19,7 @@ function App() {
   const [clipboard, setClipboard] = useState(false);
   const [networkError, setNetworkError] = useState(undefined);
   const [hasHammer, setHasHammer] = useState(false);
+  const [hasAnvil, setHasAnvil] = useState(false);
 
   const checkupWallet = async () => {  
     if(!ethereum) {
@@ -34,12 +37,18 @@ function App() {
       console.log("Account connected")
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
-      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS_THEFORGE, TheForge.abi, signer);
-      const balance = await connectedContract.balanceOf(ethAccount[0], 0);
+      const theForgeContract = new ethers.Contract(CONTRACT_ADDRESS_THEFORGE, TheForge.abi, signer);
+      const anvilContract = new ethers.Contract(CONTRACT_ADDRESS_ANVIL, Anvil.abi, signer);
+      const balanceHammer = await theForgeContract.balanceOf(ethAccount[0], 0);
+      const balanceAnvil = await anvilContract.balanceOf(ethAccount[0]);
 
-      console.log("balance for hammer ", balance, balance > 0);
-      if (balance > 0) {
+      console.log("balance for anvil ", balanceAnvil, balanceAnvil > 0);
+      if (balanceHammer > 0) {
         setHasHammer(true);
+      }
+
+      if (balanceAnvil > 0) {
+        setHasAnvil(true);
       }
 
       //setupEventListener();
@@ -109,27 +118,40 @@ function App() {
     );
   }
 
-  return (
-    <div className="bg-black text-white h-screen overflow-scroll">
-      <div className="flex flex-col pt-20">
-        <p className="text-6xl bold mb-10 text-center">The Forge summons</p>
-        <p className="text-3xl semibold mb-10 text-center">The rules</p>
-        <p className="ml-36 mb-5">Anvil ownership unlocks 3 summons per blacksmith:</p>
-        <ul className="ml-40 mb-10 list-disc list-inside">
-          <li className="mb-2">Choose wisely who you summon to The Forge.</li>
-          <li className="mb-2">The summoned person will receive a hammer NFT.</li>
-          <li className="mb-2">This hammer NFT opens an application form to join The Forge. It doesn’t grant access to The Forge.</li>
-          <li className="mb-2">Our knights review the application.</li>
-          <li className="mb-2">If the application is accepted, the person joins The Forge as an apprentice.</li> 
-          <li className="mb-2">Successful apprenticeship unlocks anvil ownership.</li>
-          <li className="mb-2">If the application isn’t accepted, the person can sell the hammer NFT on the secondary market.</li>
-        </ul>
-        <div className="mt-6 text-center">
-          <MintingHammer />
+  if (hasAnvil) {
+    return (
+      <div className="bg-black text-white h-screen overflow-scroll">
+        <div className="flex flex-col pt-20">
+          <p className="text-6xl bold mb-10 text-center">The Forge summons</p>
+          <p className="text-3xl semibold mb-10 text-center">The rules</p>
+          <p className="ml-36 mb-5">Anvil ownership unlocks 3 summons per blacksmith:</p>
+          <ul className="ml-40 mb-10 list-disc list-inside">
+            <li className="mb-2">Choose wisely who you summon to The Forge.</li>
+            <li className="mb-2">The summoned person will receive a hammer NFT.</li>
+            <li className="mb-2">This hammer NFT opens an application form to join The Forge. It doesn’t grant access to The Forge.</li>
+            <li className="mb-2">Our knights review the application.</li>
+            <li className="mb-2">If the application is accepted, the person joins The Forge as an apprentice.</li> 
+            <li className="mb-2">Successful apprenticeship unlocks anvil ownership.</li>
+            <li className="mb-2">If the application isn’t accepted, the person can sell the hammer NFT on the secondary market.</li>
+          </ul>
+          <div className="mt-6 text-center">
+            <MintingHammer />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+
+    }
+
+  return (
+      <div className="bg-black text-white h-screen overflow-scroll">
+        <div className="flex flex-col pt-20">
+          <p className="text-6xl bold mb-10 text-center">The Forge</p>
+          <p className="text-3xl semibold mb-10 text-center">You have no anvil, no hammer</p>
+          <p className="text-3xl semibold mb-10 text-center">come back when you have one you morron</p>
+        </div>
+      </div>
+  )
 }
 
-export default App;
+  export default App;
--- frontend/src/components/MintingHammer.js
+++ frontend/src/components/MintingHammer.js
@@ -7,58 +7,70 @@ const CONTRACT_ADDRESS_THEFORGE = "0xE5868B98E594510788F469ec5ca0440FCAb05873";
 
 const MintingHammer = () => {
     
-    const { ethereum } = window;
-    const [addressInvitee, setAddressInvitee] = useState("");
-    
-    const mintHammer = async () => {
-      try {
-        if (ethereum) {
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const signer = provider.getSigner();
-          const addressUser = await signer.getAddress();
-          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS_THEFORGE, TheForge.abi, signer);
-  
-          if(ethers.utils.isAddress(addressInvitee)) {
-            console.log("Going to pop wallet now to pay gas...")
-            let tx = await connectedContract.mint(addressInvitee);
-            console.log("Mining...please wait.")
-            await tx.wait()
-            console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${tx.hash}`);
-          } else (alert("Enter a valid address"));
-        } else {console.log("We need ethereum");}
-      } catch (error) {
-        console.log(error);
-      }
+  const { ethereum } = window;
+  const [addressInvitee, setAddressInvitee] = useState("");
+  const [connectedContract, setConnectedContract] = useState("");
+  const [ nbInvite, setNbInvite ] = useState("");
+
+  useEffect(() => {
+    async function getContract() {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const addressUser = await signer.getAddress();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS_THEFORGE, TheForge.abi, signer);
+
+      const balance = await contract.hasInvite(addressUser);
+
+      setConnectedContract(contract);
     }
+
+    getContract();
+  }, [])
   
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      mintHammer();
+  const mintHammer = async () => {
+    try {
+      if (ethereum) {
+        if(ethers.utils.isAddress(addressInvitee)) {
+          console.log("Going to pop wallet now to pay gas...")
+          let tx = await connectedContract.mint(addressInvitee);
+          console.log("Mining...please wait.")
+          await tx.wait()
+          console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${tx.hash}`);
+        } else (alert("Enter a valid address"));
+      } else {console.log("We need ethereum");}
+    } catch (error) {
+      console.log(error);
     }
-    
-    return (
-      <form className="">
-        <div className="bg-white rounded px-8 pt-6 pb-8 mb-4 max-w-md mx-auto sm:max-w-xl">
-          <label
-            className="block text-gray-700 text-lg font-bold mb-2"
-            htmlFor="invitation"
-          >
-            Who will you summon?
-          </label>
-          <input
-            className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 text-black rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-            type="string"
-            id="invitation"
-            onChange={(e) => setAddressInvitee(e.target.value)}
-            value={addressInvitee}
-            placeholder="Insert wallet address..."
-          />
-        </div>
-        <button className="bg-white hover:bg-gray-400 box-border h-16 w-48 text-black text-base font-bold mb-10 rounded" onClick={handleSubmit}>
-          Summon
-        </button>
-      </form>
-    )
   }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    mintHammer();
+  }
+  
+  return (
+    <form className="">
+      <div className="bg-white rounded px-8 pt-6 pb-8 mb-4 max-w-md mx-auto sm:max-w-xl">
+        <label
+          className="block text-gray-700 text-lg font-bold mb-2"
+          htmlFor="invitation"
+        >
+          Who will you summon?
+        </label>
+        <input
+          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 text-black rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="string"
+          id="invitation"
+          onChange={(e) => setAddressInvitee(e.target.value)}
+          value={addressInvitee}
+          placeholder="Insert wallet address..."
+        />
+      </div>
+      <button className="bg-white hover:bg-gray-400 box-border h-16 w-48 text-black text-base font-bold mb-10 rounded" onClick={handleSubmit}>
+        Summon
+      </button>
+    </form>
+  )
+}
 
 export default MintingHammer;
